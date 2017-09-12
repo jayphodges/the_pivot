bc9df8794e7bcbb8cb63a62195f3b43c33e6e7ed
randy-darsh <rspringer47@gmail.com>
2017-09-12 16:50:11 -0600

    add category item relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912223925) do
+ActiveRecord::Schema.define(version: 20170912224420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20170912223925) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
   end
 
+  add_foreign_key "items", "categories"
 end
