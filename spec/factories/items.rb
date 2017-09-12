5748570da0952073e52f8abb683a6eb688dd4238
amhursh <amhursh@gmail.com>
2017-09-12 15:36:41 -0600

    Update item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,17 @@
 FactoryGirl.define do
   factory :item do
-    title "MyString"
-    description "MyString"
-    price 1.5
-    image "MyString"
+    
+    sequence :title do |i|
+    	"title#{i}"
+    end
+
+    sequence :description do |i|
+    	"descrip#{i}"
+    end
+    
+    price rand(1.0..1000.0)
+    
+    image "http://lorempixel.com/400/200"
+    
   end
 end
