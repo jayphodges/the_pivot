d9494de6fa2df447cd3a80c838264a54746d82b8
randy-darsh <rspringer47@gmail.com>
2017-09-12 17:33:10 -0600

    pass story 3

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
 
   def show
-    @categories = Category.find_by(title: params[:id])
+    @category = Category.find_by(title: params[:id])
   end
 end
