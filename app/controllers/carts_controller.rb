a98a34c3a5b763776849d58d8006ba06e307adc5
JoelLindow <joellindow@gmail.com>
2017-09-12 18:30:47 -0600

    Created Cart Controller, Carts Resources (paths)

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,6 @@
+class CartsController < ApplicationController
+
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
 	resources :items, only: [:index]
+
+	resources :carts
+
 	resources :categories, path: "/", only: [:show]
 
 end
