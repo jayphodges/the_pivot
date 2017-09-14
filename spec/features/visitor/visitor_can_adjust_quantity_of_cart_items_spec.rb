da5d6deab90e2f477e5b95ed501681e5c078623d
amhursh <amhursh@gmail.com>
2017-09-14 14:01:20 -0600

    Add reduce quanitity testing

--- spec/features/visitor/visitor_can_adjust_quantity_of_cart_items_spec.rb
+++ spec/features/visitor/visitor_can_adjust_quantity_of_cart_items_spec.rb
@@ -20,12 +20,70 @@ RSpec.feature "User increases cart quantity of item" do
     click_on "Update"
 
     expect(current_path).to eq(cart_path)
-    save_and_open_page
 
     within(".item-#{item.id}") do
       expect(page).to have_field("Quantity", with: "2")
     end
+
     expect(page).to have_content("Item Subtotal: $#{item.price * 2}")
     expect(page).to have_content("Total: #{item.price * 2}")
   end
+
+  scenario "User decreases quantity of items in the cart" do
+    category = create(:category)
+    item = create(:item, category: category)
+    item2 = create(:item, category: category)
+    visit '/items'
+
+    within(".index_item_#{item.id}") do
+      click_on "Add to Cart"
+    end
+
+    within(".index_item_#{item2.id}") do
+      click_on "Add to Cart"
+    end    
+
+    click_on "View Cart"
+
+    within(".item-#{item.id}") do
+      expect(page).to have_field("Quantity", with: "1")
+    end
+
+    within(".item-#{item2.id}") do
+      expect(page).to have_field("Quantity", with: "1")
+    end
+
+    expect(page).to have_content("Total: #{item2.price + item.price}")
+
+    save_and_open_page
+
+    within(".item-#{item.id}") do
+      fill_in "Quantity", with: "0"
+      click_on "Update"
+    end
+
+    expect(current_path).to eq cart_path
+
+    within(".item-#{item.id}") do
+      expect(page).to have_field("Quantity", with: "0")
+      expect(page).to have_content("Item Subtotal: $0.00")
+    end
+
+    within(".item-#{item2.id}") do
+      expect(page).to have_field("Quantity", with: "1")
+      expect(page).to have_content("Item Subtotal: #{item2.price}")
+    end
+
+    expect(page).to have_content("Total: $#{item2.price}")
+  end
+
+# -As a visitor
+# -When I visit “/cart”
+# -Then I should see my item with a quantity of 1
+# -When I decrease the quantity
+# -Then my current page should be ‘/cart’
+# -And that item’s quantity should reflect the decrease
+# -And the subtotal for that item should decrease
+# -And the total for the cart should match that decrease
+
 end
