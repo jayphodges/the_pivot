c195b193f6d6c5c0019be11b751b7c3ca1bbd93e
Sam Lim <limsammy1@gmail.com>
2017-09-14 17:43:14 -0600

    add_item spec passing

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,11 +2,15 @@ describe Cart do
   context "instance methods" do
     it '.add_item' do
       c = Category.create(title: "Guitars")
-      Item.create(title: 'title', description: "desc",
+      item = Item.create(title: 'title', description: "desc",
         price: 5.5, image: 'foo.png', category_id: c.id)
       cart = Cart.new({})
 
       expect(cart.contents).to eq({})
+
+      cart.add_item(item.id)
+
+      expect(cart.contents).to eq({item.id.to_s => 1})
     end
   end
 end
