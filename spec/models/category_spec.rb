4a0fca5d1aaa82711091a36c0fdaf62387fe808c
Sam Lim <limsammy1@gmail.com>
2017-09-14 17:25:32 -0600

    Uniqueness category spec passing

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe Category, type: :model do
 
         expect(category).to be_invalid
       end
+
+      it 'is invalid if not unique' do
+        Category.create(title: "Guitars")
+        category = Category.create(title: "Guitars")
+
+        expect(category).to be_invalid
+      end
     end
 
     context "valid attributes" do
