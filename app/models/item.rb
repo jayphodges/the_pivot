d838b18c240487ed295f7b0371c66a6666b6dbf4
Sam Lim <limsammy1@gmail.com>
2017-09-14 17:34:05 -0600

    Most validation specs passing. Need to test validity

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :description, :price, :image, :category_id, presence: true
+
   belongs_to :category
 
   def formatted_price
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it 'is invalid without a title' do
+        category = Category.create(title: "Guitars")
+        item = Item.create(description: "desc",
+          price: 5.5, image: 'foo.png', category_id: category.id)
+
+        expect(item).to be_invalid
+      end
+
+      it 'is invalid without a description' do
+        category = Category.create(title: "Guitars")
+        item = Item.create(title: "title",
+          price: 5.5, image: 'foo.png', category_id: category.id)
+
+        expect(item).to be_invalid
+      end
+
+      it 'is invalid without a price' do
+        category = Category.create(title: "Guitars")
+        item = Item.create(title: 'title', description: "desc",
+          image: 'foo.png', category_id: category.id)
+
+        expect(item).to be_invalid
+      end
+
+      it 'is invalid without an image' do
+        category = Category.create(title: "Guitars")
+        item = Item.create(title: 'title', description: "desc",
+          price: 5.5, category_id: category.id)
+
+        expect(item).to be_invalid
+      end
+
+      it 'is invalid without a category' do
+        category = Category.create(title: "Guitars")
+        item = Item.create(title: 'title', description: "desc",
+          price: 5.5, image: 'foo.png')
+
+        expect(item).to be_invalid
+      end
+
+      it 'is invalid if title is not unique' do
+        c = Category.create(title: "Guitars")
+        Item.create(title: 'title', description: "desc",
+          price: 5.5, image: 'foo.png', category_id: c.id)
+        item = Item.create(title: 'title', description: "desc",
+          price: 5.5, image: 'foo.png', category_id: c.id)
+
+        expect(item).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      # it "is valid with valid attributes" do
+      #   category = create(:category)
+
+      #   expect(category).to be_valid
+      # end
+    end
+  end
 end
