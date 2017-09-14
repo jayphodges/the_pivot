ea99c0b7dc34680d990cc2009a53188b6aa755be
Sam Lim <limsammy1@gmail.com>
2017-09-14 17:21:16 -0600

    Set up category model spec and validations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it 'is invalid without a title' do
+        category = Category.create(title: "")
+
+        expect(category).to be_invalid
+      end
+    end
+  end
 end
