dbeef3499d46072d55e6d571ca0d6eeb958c5905
Sam Lim <limsammy1@gmail.com>
2017-09-14 17:46:45 -0600

    update item spec passing

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,16 +1,25 @@
 describe Cart do
   context "instance methods" do
-    it '.add_item' do
+    before(:each) do
       c = Category.create(title: "Guitars")
-      item = Item.create(title: 'title', description: "desc",
+      @item = Item.create(title: 'title', description: "desc",
         price: 5.5, image: 'foo.png', category_id: c.id)
-      cart = Cart.new({})
+      @cart = Cart.new({})
+    end
 
-      expect(cart.contents).to eq({})
+    it '.add_item' do
+      expect(@cart.contents).to eq({})
 
-      cart.add_item(item.id)
+      @cart.add_item(@item.id)
 
-      expect(cart.contents).to eq({item.id.to_s => 1})
+      expect(@cart.contents).to eq({@item.id.to_s => 1})
+    end
+
+    it '.update_item' do
+      @cart.add_item(@item.id)
+      @cart.update_item(@item.id, 12)
+
+      expect(@cart.contents).to eq({@item.id.to_s => 12})
     end
   end
 end
