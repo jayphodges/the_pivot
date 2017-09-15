b7d98f771b062e33e81c857f05cb023a697f43e1
amhursh <amhursh@gmail.com>
2017-09-15 01:25:33 -0600

    Refactor

--- spec/features/user/user_can_log_out_spec.rb
+++ spec/features/user/user_can_log_out_spec.rb
@@ -4,13 +4,19 @@ describe "User can log out" do
 	context "user can click link to log out" do
 		scenario "user is logged out and cannot see logout link" do
 			user = create(:user)
-			allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+			
+			visit root_path
 
-			visit items_path
+			click_on "Login"
+
+			expect(current_path).to eq login_path
+
+			fill_in "session[username]", with: user.username
+			fill_in "session[password]", with: user.password
+			click_button "Login"
 
 			click_on "Logout"
 
-			expect(current_path).to eq root_path
 			expect(page).to have_content("Login")
 			expect(page).to_not have_content("Logout")
 		end
