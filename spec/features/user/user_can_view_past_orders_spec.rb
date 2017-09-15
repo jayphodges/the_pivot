62ab811ff7fe337c8977ea30591a5930d89a08c9
amhursh <amhursh@gmail.com>
2017-09-15 16:19:55 -0600

    Refactor factory_girl

--- spec/features/user/user_can_view_past_orders_spec.rb
+++ spec/features/user/user_can_view_past_orders_spec.rb
@@ -4,10 +4,10 @@ describe "User can see past orders" do
 	context "User visits /orders" do
 		scenario "User can see all orders" do
 			user = create(:user)
-			user.orders.create_list(:order, 3)
+			order1, order2, order3 = create_list(:order, 3, user: user)
 
 			allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
+			binding.pry
 			visit '/orders'
 
 			expect(page).to have_content(order.first.id)
