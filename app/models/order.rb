862e85db63368bf76fbb3cc92e34291f04a729ed
amhursh <amhursh@gmail.com>
2017-09-15 17:16:50 -0600

    Add orders_items join table and set up relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :orders_items
+  has_many :items, through: :orders_items
 end
---
+++ spec/factories/orders_items.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :orders_item do
+    item nil
+    order nil
+  end
+end
