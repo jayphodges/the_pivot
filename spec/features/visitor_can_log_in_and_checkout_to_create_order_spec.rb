251d3eefb2c4f98d52fe1379e20385e1301683ef
amhursh <amhursh@gmail.com>
2017-09-16 15:32:58 -0600

    Remove comments

--- spec/features/visitor_can_log_in_and_checkout_to_create_order_spec.rb
+++ spec/features/visitor_can_log_in_and_checkout_to_create_order_spec.rb
@@ -39,20 +39,4 @@ describe "Visitor can log in and create order" do
   end
 end
 
-# Background: An existing user and a cart with items
-
-# -As a visitor
-# -When I add items to my cart
-# -And I visit “/cart”
-# -And I click “Login or Register to Checkout”
-# -Then I should be required to login
-
-# -When I am logged in
-# -And I visit my cart
-# -And when I click “Checkout”
-# -Then the order should be placed
-# -And my current page should be “/orders”
-# -And I should see a message “Order was successfully placed”
-# -And I should see the order I just placed in a table
-
 
