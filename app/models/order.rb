340c22398cb4f37f1b41023993cd407053a30f27
amhursh <amhursh@gmail.com>
2017-09-15 18:38:07 -0600

    Add item_quantity and item_subtotal methods

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,15 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :orders_items
   has_many :items, through: :orders_items
+
+  def item_quantity(item)
+  	id = item.id
+  	items.where(id: id).count
+  end
+
+  def item_subtotal(item)
+  	id = item.id
+  	items.where(id: id).sum(:price)
+  end
+
 end
