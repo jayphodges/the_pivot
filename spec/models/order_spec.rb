a3e6bacbbd5f66d8bdf80183c321092fb5bd1a4a
Sam Lim <limsammy1@gmail.com>
2017-09-16 13:42:26 -0600

    Instance methods total_order_price and item_quantity specs passing

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,27 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  context "invalid attributes" do
-    it 'is invalid without a user id' do
-      order = Order.create(status: 1)
+  describe "validations" do
+    context "invalid attributes" do
+      it 'is invalid without a user id' do
+        order = Order.create(status: 1)
 
-      expect(order).to be_invalid
+        expect(order).to be_invalid
+      end
+
+      it 'is invalid without a status' do
+        user = create(:user)
+        order = Order.create(user_id: user.id, status: "")
+
+        expect(order).to be_invalid
+      end
     end
 
-    it 'is invalid without a status' do
-      user = create(:user)
-      order = Order.create(user_id: user.id, status: "")
+    context "valid attributes" do
+      it 'is valid with a status and user_id' do
+        user = create(:user)
+        order = create(:order, user_id: user.id)
 
-      expect(order).to be_invalid
+        expect(order).to be_valid
+      end
     end
   end
 
-  context "valid attributes" do
-    it 'is valid with a status and user_id' do
-      user = create(:user)
-      order = create(:order, user_id: user.id)
+  describe "methods" do
+    context "instance methods" do
+      it '.total_order_price' do
+        user = create(:user)
+        order = create(:order, user: user, status: 1)
+        category = create(:category)
+        item_1 = order.items.create(title: "Cool Item", description: "Descrip",
+                                    price: 35.0, image: "http://lorempixel.com/400/200",
+                                    category: category)
+        item2 = order.items.create(title: "Cool Item2", description: "Descrip2",
+                                   price: 35.50, image: "http://lorempixel.com/400/200",
+                                   category: category)
+        item3 = order.items.create(title: "Cool Item3", description: "Descrip3",
+                                   price: 35.99, image: "h`ttp://lorempixel.com/400/200",
+                                   category: category)
 
-      expect(order).to be_valid
+        expect(order.total_order_price).to eq(106.49)
+      end
+
+      it '.item_quantity1' do
+        user = create(:user)
+        order = create(:order, user: user, status: 1)
+        category = create(:category)
+        item_1 = order.items.create(title: "Cool Item", description: "Descrip",
+                                    price: 35.0, image: "http://lorempixel.com/400/200",
+                                    category: category)
+        item2 = order.items.create(title: "Cool Item2", description: "Descrip2",
+                                   price: 35.50, image: "http://lorempixel.com/400/200",
+                                   category: category)
+        item3 = order.items.create(title: "Cool Item3", description: "Descrip3",
+                                   price: 35.99, image: "h`ttp://lorempixel.com/400/200",
+                                   category: category)
+        4.times do
+          order.items << item_1
+        end
+
+        expect(order.item_quantity(item_1)).to eq(5)
+      end
     end
   end
 end
