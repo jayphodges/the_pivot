8870725de4b26215f62b790605b398b441333531
randy-darsh <rspringer47@gmail.com>
2017-09-17 17:10:54 -0600

    add admin edit link. add test portion so admin cannot edit other users

--- spec/features/admin/admin_cannot_modify_users_spec.rb
+++ spec/features/admin/admin_cannot_modify_users_spec.rb
@@ -3,7 +3,7 @@ describe 'Admin cannot edit user information' do
     context 'admin can visit their edit page' do
       context 'admin can edit their information' do
         scenario 'admin cannot edit other users information' do
-
+          user = create(:user, id: 5, address: 'something', full_name: 'something else')
           admin = create(:user, role: 1)
 
           visit root_path
@@ -18,7 +18,7 @@ describe 'Admin cannot edit user information' do
 
           expect(current_path).to eq admin_dashboard_path
 
-          click_on "Edit User"
+          click_on "Edit Admin"
           expect(current_path).to eq("/users/#{admin.id}/edit")
 
           fill_in "Full Name", with: "Joel Lindow"
@@ -29,6 +29,20 @@ describe 'Admin cannot edit user information' do
           expect(current_path).to eq(dashboard_path)
           expect(page).to have_content("1122 Booger Booger Avenue")
           expect(page).to have_content("Joel Lindow")
+
+          visit("/users/#{user.id}/edit")
+
+          fill_in "Full Name", with: "some dude"
+          fill_in "Address", with: "some address"
+
+          click_on "Update"
+
+          visit("/users/#{user.id}")
+
+          save_and_open_page
+
+          expect(page).to_not have_content("some dude")
+          expect(page).to_not have_content("some address")
         end
       end
     end
