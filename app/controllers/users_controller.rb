ca7774740748d8834fbc03768f394773325596cf
randy-darsh <rspringer47@gmail.com>
2017-09-17 16:08:03 -0600

    change update to redirect to dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def update
     user = current_user
     if @user.update(user_params)
-      redirect_to user_path(@user)
+      redirect_to dashboard_path
     else
       render :edit
     end
