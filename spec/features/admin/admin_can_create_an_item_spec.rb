31e18cb17ff57858adf6d84bf8b4b1f9d881a063
Sam Lim <limsammy1@gmail.com>
2017-09-17 15:49:18 -0600

    All specs passing for admin item create function

--- spec/features/admin/admin_can_create_an_item_spec.rb
+++ spec/features/admin/admin_can_create_an_item_spec.rb
@@ -23,17 +23,12 @@ describe "Admin can add items" do
       fill_in "Image", with: 'foo.png'
 
       click_on "Submit"
+
+      expect(page).to have_content("A Product has been created.")
+
+      visit items_path
+
+      expect(page).to have_content("A Product")
     end
   end
 end
-
-
-  # -As an authenticated Admin:
-  # -I can create an item.
-
-  # An item must have a title, description and price.
-  # An item must belong to at least one category.
-  # The title and description cannot be empty.
-  # The title must be unique for all items in the system.
-  # The price must be a valid decimal numeric value and greater than zero.
-  # The photo is optional. If not present, a stand-in photo is used. (PAPERCLIP)
