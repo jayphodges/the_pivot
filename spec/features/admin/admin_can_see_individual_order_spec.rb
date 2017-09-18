ef8d264c220e4f33cd28c80b0d961513fe025b2f
amhursh <amhursh@gmail.com>
2017-09-18 15:29:53 -0600

    Refactor test, remove comments

--- spec/features/admin/admin_can_see_individual_order_spec.rb
+++ spec/features/admin/admin_can_see_individual_order_spec.rb
@@ -24,25 +24,22 @@ describe "Admin can view individual order" do
 			  													 category: category)
 
 	      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-# -As an authenticated Admin, when I visit an individual order page
+
 	      visit order_path(order1)
 
-	      save_and_open_page
-# -Then I can see the order’s date and time.
 	      expect(page).to have_content("Order #{order1.id}")
 	      expect(page).to have_content("Order Submitted: #{order1.created_at.to_formatted_s(:long_ordinal)}")
 	      expect(page).to have_content("Order #{order1.status} at #{order1.updated_at.to_formatted_s(:long_ordinal)}")
-# -And I can see the purchaser’s full name and address.
+
         expect(page).to have_content("Customer Name: #{user.full_name}")
         expect(page).to have_content("Mailing Address: #{user.address}")
-# -And I can see, for each item on the order:
-# The item’s name, which is linked to the item page.
-        expect(page).to have_selector(:link_or_button, "item1.title")
-# Quantity in this order.
+
+        expect(page).to have_selector(:link_or_button, "#{item1.title}")
+
         expect(page).to have_content(order1.item_quantity(item1))
-# Price
+
         expect(page).to have_content("$35.00 Each")
-# Line item subtotal. And I can see the total for the order. And I can see the status for the order.
+
         expect(page).to have_content("Subtotal: $35.50")
         expect(page).to have_content("Total Price: $106.49")
       end
