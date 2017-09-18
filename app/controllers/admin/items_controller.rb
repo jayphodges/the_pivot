2057bd6c939f89c7aa076d64a2210890493f6106
randy-darsh <rspringer47@gmail.com>
2017-09-18 16:41:07 -0600

    add edit and update method to controller

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -13,14 +13,29 @@ class Admin::ItemsController < Admin::BaseController
       flash[:success] = "#{@item.title} has been created."
       redirect_to item_path(@item)
     else
-      flash[:message] = "User Profile not created"
-      redirect_to new_user_path
+      flash[:message] = "Item has not been created"
+      redirect_to new_admin_item_path
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:success] = "#{@item.title} has been updated."
+      redirect_to admin_items_path
+    else
+      flash[:message] = "Item has not been updated"
+      redirect_to edit_admin_item_path
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:title, :description, :price, :category_id, :image)
+    params.require(:item).permit(:title, :description, :price, :category_id, :image, :status)
   end
 end
