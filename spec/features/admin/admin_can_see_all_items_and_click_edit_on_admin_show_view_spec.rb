1a5b7f2a7405480dcf3465ba1f70c51eb2806ab3
JoelLindow <joellindow@gmail.com>
2017-09-18 13:45:48 -0600

    removed comments from test

--- spec/features/admin/admin_can_see_all_items_and_click_edit_on_admin_show_view_spec.rb
+++ spec/features/admin/admin_can_see_all_items_and_click_edit_on_admin_show_view_spec.rb
@@ -28,17 +28,3 @@ describe "Admin visits /admin/dashboard" do
     end
   end
 end
-
-# -As an Admin
-# -When I visit “/admin/dashboard”
-# -Then I should see a link for viewing all items
-# -And when I click that link
-# -Then my current path should be “/admin/items”
-# -Then I should see a table with all items (active and inactive)
-# -And each item should have:
-#
-# A thumbnail of the image
-# Title that links to the item
-# Description
-# Status
-# Actions (Edit)
