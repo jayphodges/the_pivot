ba21edc44acffd0e6458451a93da6fd5350f3857
amhursh <amhursh@gmail.com>
2017-09-17 23:26:51 -0600

    Remove bad routes and add single orders update

--- config/routes.rb
+++ config/routes.rb
@@ -10,16 +10,12 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :orders, only: [:index, :show, :create]
+  resources :orders, only: [:index, :show, :create, :update]
 
   namespace :admin do
     get '/dashboard', to: 'admin#show'
     resources :items, only: [:new, :create]
   end
-
-  patch '/order', to: 'orders#cancel', as: 'cancel_order'
-  patch '/order', to: 'orders#pay', as: 'mark_paid_order'
-  patch '/order', to: 'orders#complete', as: 'mark_completed_order'
 
   get '/dashboard', to: 'users#show'
 
