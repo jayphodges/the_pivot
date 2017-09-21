3cf536b740a9cdff89af505a412c547ab021c00e
amhursh <amhursh@gmail.com>
2017-09-20 20:37:44 -0600

    Remove comments

--- spec/features/visitor/items/visitor_can_view_retired_items_spec.rb
+++ spec/features/visitor/items/visitor_can_view_retired_items_spec.rb
@@ -31,10 +31,3 @@ describe "Visitor can visit retired item page" do
     end
   end
 end
-
-
-
-# -As a user if I visit an item page and that item has been retired
-# -Then I should still be able to access the item page
-# -And I should not be able to add the item to their cart
-# -And I should see in place of the “Add to Cart” button or link - “Item Retired”
