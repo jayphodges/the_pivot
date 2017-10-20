64d70c003c8d1cb098182faa02f0e0968d87596e
Daniel Phillips <danieledward.phillips@gmail.com>
2017-10-20 08:50:42 -0600

    start bm testing

---
+++ spec/features/business_manager/manage_items_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+#As a business manager I should be able to:
+#Manage items on my business
+#When I visit ‘/:store-name/admin/items’
+#I should see a list of items
+#I should see an edit item button next to each item
+#When I click on the edit button
+#My path should be ‘/items/:id/edit’
+#I should see form fields for all item attributes
+#When I click submit
+#My path should be ‘/items/:id’
+#I should see the new edits to the item
+
+
+
---
+++ spec/features/business_manager/manage_orders_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+#Manage orders on my business
+#When I visit ‘/:store-name/orders’
+#I should see a list of orders for the store
+#When i click on the edit button next to an order
+#I should be taken to /order/:id
+#I should see fields to edit the attributes
+#When I change the attributes and click submit
+#I should see the changes I have made
