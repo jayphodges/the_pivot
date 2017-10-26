021bfdca874ffc5babbcb357fabd48d7cd2e69f3
jayphodges <jayphodges@gmail.com>
2017-10-25 18:35:29 -0600

    Activerecord logic for endpoints completed

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -9,20 +9,44 @@ class Api::V1::OrdersController < ApplicationController
     render json: Order.orders_by_day_of_week
   end
 
-  # Average Order price
+  # Most expensive orders
   def most_expensive_orders
-    Order.merge(Order.completed).joins(:orders_items).group('orders.id').order('sum_unit_price DESC').sum(:unit_price)
+    Order.merge(Order.completed)
+    .joins(:orders_items)
+    .group('orders.id')
+    .order('sum_unit_price DESC')
+    .sum(:unit_price)
   end
   # Top selling merchants / by revenue
   def top_selling_merchant
-    Store
+    Store.joins(orders: [:orders_items])
+    .group('stores.id')
+    .order('sum_unit_price DESC')
+    .sum(:unit_price)
   end
-  # top selling items
-
-  # top item by revenue
-
-  # Most active customers
-
-  #
+  # top selling items by revenue
+  def top_selling_items
+    Item.joins(:orders_items, :orders)
+    .merge(Order.completed).group('items.id')
+    .order('sum_orders_items_unit_price DESC')
+    .sum('orders_items.unit_price')
+  end
+  # most sold items
+  def most_sold_items
+    Item.joins(:orders_items, :orders)
+    .merge(Order.completed)
+    .group('items.id')
+    .order('count_items_id DESC')
+    .count('items.id')
+  end
+  # Most active customers by orders
+  def most_active_customer
+    User.joins(orders: [:orders_items])
+    .merge(Order.completed)
+    .group('users.id')
+    .order('sum_orders_items_unit_price DESC')
+    .sum('orders_items.unit_price')
+  end
+  # Average order price
 
 end
